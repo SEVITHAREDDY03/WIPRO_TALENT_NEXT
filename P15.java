30a24694f2312e8e50f99cbdab06338c7d7a567a
SEVITHAREDDY03 <58474492+SEVITHAREDDY03@users.noreply.github.com>
2019-12-04 22:53:40 +0530

    Update P15.java

--- P15.java
+++ P15.java
@@ -5,29 +5,30 @@ class P15
       public static void main(String args[])
         {
           Scanner sc=new Scanner(System.in);
+          System.out.println("Enter number");
            int n=sc.nextInt();
            if(n<=1)
-           System.out.println("Not a prime");
+           System.out.println("Not Prime");
            else
              {
-                int flag=0;
+                int i,c=0;
               if(n==2)
                {
-                System.out.println("prime");
+                System.out.println("Prime");
                }               
               else
                 {
-                 for(int i=2;i<=Math.sqrt(n);i++)
+                 for(i=2;i<=Math.sqrt(n);i++)
                    {
                      if(n%i==0)
                        {
-                         System.out.println("Not a prime");
-                         flag=1;
+                         System.out.println("Not Prime");
+                         c=1;
                          break;
                         }
                      }
-                    if(flag==0)
-                      System.out.println("prime");
+                    if(c==0)
+                      System.out.println("Prime");
                    }
                   }
                }
