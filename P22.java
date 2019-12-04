f18894ab450f3f31597e7aa17cb93a5df48c9f3f
SEVITHAREDDY03 <58474492+SEVITHAREDDY03@users.noreply.github.com>
2019-12-04 22:59:33 +0530

    Update P22.java

--- P22.java
+++ P22.java
@@ -1,15 +1,15 @@
 import java.util.*;
-class P221
+class P22
    {
     public static void main(String args[])
       { 
-         int c=5,i=2;
-         while(c>0)
+         int count=5,i=2;
+         for(count=0;count<5;)
            {
-            if(i%2==0&&i%3==0&&i%5==0)
+            if(i%2==0 && i%3==0 && i%5==0)
             {
               System.out.println(i);
-              c--;
+              count++;
              }
             i++;
             }
