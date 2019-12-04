4729e3137ec3c637337d07d7a10815d2e4ddda2f
SEVITHAREDDY03 <58474492+SEVITHAREDDY03@users.noreply.github.com>
2019-12-04 22:56:23 +0530

    Update P19.java

--- P19.java
+++ P19.java
@@ -3,19 +3,22 @@ class P19
   {
    public static void main(String args[])
      {
-       if(args.length!=0)
+       if(args.length==0)
         {
+         System.out.println("Please enter an integer number");
+        }
+       else
+      {
+         int i,j;
        int n=Integer.parseInt(args[0]);
-       for(int i=1;i<=n;i++)
-          {
-           for(int j=1;j<=i;j++)
+       for(i=1;i<=n;i++)
+       {
+           for(j=1;j<=i;j++)
              {
-               System.out.print("*");
+               System.out.print("* ");
              }
            System.out.println();
         }
        }
-        else
-          System.out.println("Please enter an integer number ");
-    }
+    }    
 }
